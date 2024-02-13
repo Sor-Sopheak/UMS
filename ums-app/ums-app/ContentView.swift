71752a9782b20e4f5870e43eb8e6f916324c374b
SOR SOPHEAK <ssopheak2002@gmail.com>
2024-02-13 18:34:37 +0700

    Setup project structure

---
+++ ums-app/ums-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ums-app
+//
+//  Created by SOR SOPHEAK on 13/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ums-app/ums-app/ums_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ums_appApp.swift
+//  ums-app
+//
+//  Created by SOR SOPHEAK on 13/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ums_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
